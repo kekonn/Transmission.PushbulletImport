af40aa57c36a9c38634c4f9091836f0f803f8b84
Jonas Van der Aa <ketsukonn@gmail.com>
2019-08-13 23:27:01 +0200

    added Configuration singleton

---
+++ Configuration/Configuration.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Transmission.PushbulletImport.Configuration
+{
+    public sealed class Configuration
+    {
+        private const string ConfigFileName = "appsettings.json";
+        
+        public static Configuration Default { get; private set; }
+        
+        public IConfigurationRoot ApplicationConfig { get; private set; }
+        
+        static Configuration()
+        {
+            Default = new Configuration(ConfigFileName);
+        }
+
+        private Configuration(string settingsFile)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(ConfigFileName)
+                .AddEnvironmentVariables(prefix: "TPI_")
+                .Build();
+
+            ApplicationConfig = config;
+        }
+    }
+}
