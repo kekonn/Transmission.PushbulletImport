12e06ddccacfeade9fbc31de550225986e6b9bcb
Jonas Van der Aa <ketsukonn@gmail.com>
2019-08-15 09:58:41 +0200

    removed unused statements in Program.cs, added first configuration description draft

--- Commands/Pull.cs
+++ Commands/Pull.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
 using CommandLine;
+using PushbulletSharp.Filters;
+using PushbulletSharp.Models.Responses;
 using CFG = Transmission.PushbulletImport.Configuration;
 
 namespace Transmission.PushbulletImport.Commands
@@ -9,15 +12,50 @@ namespace Transmission.PushbulletImport.Commands
         [Verb("pull", HelpText = "Pulls in new messages")]
         public class PullOptions
         {
-            
+            [Option('s',"since", Required = false)]
+            public DateTime Since { get; set; } = DateTime.Now.AddMinutes(-30);
         }
 
         public static int Execute(PullOptions options)
         {
-            var pbClient = CFG.Configuration.Default.PushbulletClient;
+            var pbClient = CFG.Configuration.Default.PBClient;
+
+            var pushFilter = new PushResponseFilter()
+            {
+                ModifiedDate = options.Since,
+                IncludeTypes = new[] { PushResponseType.File, PushResponseType.Link}
+            };
             
-            
-            
+            var pushes = pbClient.GetPushes(pushFilter).Pushes;
+
+            pushes.ForEach(ProcessPush);
+
+            return Program.ExitNormal;
+        }
+
+        private static void ProcessPush(PushResponse push)
+        {
+            switch (push.Type)
+            {
+                case PushResponseType.File:
+                    ProcessTorrentFile(push);
+                    return;
+                case PushResponseType.Link:
+                    ProcessMagnetLink(push);
+                    return;
+                default:
+                    // we shouldn't even be here, because of the push filter, so just ignore this
+                    return;
+            }
+        }
+
+        private static void ProcessMagnetLink(PushResponse push)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ProcessTorrentFile(PushResponse push)
+        {
             throw new NotImplementedException();
         }
     }
---
+++ configuration.md
@@ -0,0 +1,15 @@
+﻿# Settings
+## appsettings.json
+
+This file  is to be placed in the applications main directory.
+
+### Section "Pushbullet"
+* "Pushbullet API key": String containing the Pushbullet API key created for the app.
+* "Channel tag": String containing the channel tag where PB should listen
+
+## Environment variables
+
+All Environment Variables pertaining to the importer have to be prefixed with "TPI_".
+
+* TPI_PBAPI: Pusbhullet API key
+* TPI_CHANNEL: Pushbullet channel tag
